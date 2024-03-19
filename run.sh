6069d5b2dcc9a72ffc50ce593435cc6ef1c953cc
sumardirusdan <161239342+sumardirusdan@users.noreply.github.com>
2024-03-20 05:03:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-48ac330f-ed38-4146-be6a-596ce84ed7a4/)
 
 
 echo "$curl_output" >> Update.tsx
